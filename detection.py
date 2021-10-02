8454a0c58c090831ebbae7a9ca166c9ed19e1b06
mrobert3456 <mrobert3456@gmail.com>
2021-10-02 17:06:57 +0200

    Added curvature calculator GetCurv method.

--- detection.py
+++ detection.py
@@ -281,11 +281,69 @@ def draw_lines(img, img_w, left_fit, right_fit, perspective):
     result = cv.addWeighted(img, 1, newwarp, 0.2, 0)
 
     color_warp_lines = np.dstack((warp_zero, warp_zero, warp_zero))
-    cv.polylines(color_warp_lines, np.int_([pts_right]), isClosed=False, color=(255, 255, 0), thickness=25) #right lane line
-    cv.polylines(color_warp_lines, np.int_([pts_left]), isClosed=False, color=(0, 0, 255), thickness=25) #left lane line
-    newwarp_lines = perspective_transform(color_warp_lines, perspective[1], perspective[0]) # inverz persp. transform
-    result = cv.addWeighted(result, 1, newwarp_lines, 1, 0) # combine the result with the result image
+    cv.polylines(color_warp_lines, np.int_([pts_right]), isClosed=False, color=(255, 255, 0),
+                 thickness=25)  # right lane line
+    cv.polylines(color_warp_lines, np.int_([pts_left]), isClosed=False, color=(0, 0, 255),
+                 thickness=25)  # left lane line
+    newwarp_lines = perspective_transform(color_warp_lines, perspective[1], perspective[0])  # inverz persp. transform
+    result = cv.addWeighted(result, 1, newwarp_lines, 1, 0)  # combine the result with the result image
+
     return result
+
+
+def GetCurv(result, img, ploty, left_fit, right_fit, left_fitx, right_fitx, goodlane):
+    y_eval = np.max(ploty)
+
+    left_curverad = ((1 + (2 * left_fit[0] * y_eval + left_fit[1]) ** 2) ** 1.5) / np.absolute(2 * left_fit[0])
+    right_curverad = ((1 + (2 * right_fit[0] * y_eval + right_fit[1]) ** 2) ** 1.5) / np.absolute(2 * right_fit[0])
+
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30 / 720  # meters per pixel in y dimension
+    xm_per_pix = 3.7 / 640  # meters per pixel in x dimension
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty * ym_per_pix, left_fitx * xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty * ym_per_pix, right_fitx * xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2 * left_fit_cr[0] * y_eval * ym_per_pix + left_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * left_fit_cr[0])
+
+    right_curverad = ((1 + (2 * right_fit_cr[0] * y_eval * ym_per_pix + right_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
+        2 * right_fit_cr[0])
+    # Now our radius of curvature is in meters
+
+    diff = np.abs(left_curverad-right_curverad)
+    #print(diff)
+
+    radius = round((float(left_curverad) + float(right_curverad)) / 2., 2)
+    #radius = right_curverad/left_curverad
+
+    lane_width = (right_fit[2] - left_fit[2]) * xm_per_pix
+    #lane_width = np.mean((right_fitx-left_fitx)*xm_per_pix)
+    print(lane_width)
+
+    center = (right_fit[2] - left_fit[2]) / 2
+
+    #diff = np.abs(left_curverad-right_curverad)
+    #if diff>100:
+     #   goodlane=False
+
+    #if lane_width>4 or lane_width<3:
+     #   goodlane=False
+
+    left_off = (center - left_fit[2]) * xm_per_pix
+    right_off = (right_fit[2] - center) * xm_per_pix
+
+    center_off = round(center - img.shape[0] / 2. * xm_per_pix, 2)
+
+
+    # print to image
+    text = "radius = %s [m]\noffcenter = %s [m]" % (str(right_curverad), str(left_curverad))
+    for i, line in enumerate(text.split('\n')):
+        i = 50 + 20 * i
+        cv.putText(result, line, (0, i), cv.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1, cv.LINE_AA)
+
+    return goodlane,radius, lane_width, center_off
 
 
 def process_adv(image):
@@ -294,17 +352,26 @@ def process_adv(image):
 
     combined_img = thresholding_pipeline(image)
     roi_image = region_of_interest(combined_img)
-    blurred = cv.medianBlur(roi_image, 5)
+    blurred = cv.medianBlur(roi_image,3)
 
     warped = perspective_transform(blurred, s_mask, dest_mask)
 
-    left_fit, right_fit = sliding_windown(warped) # returns the right and the left lane lines points
+    left_fit, right_fit = sliding_windown(warped)  # returns the right and the left lane lines points
     result = draw_lines(image, warped, left_fit, right_fit, perspective=[s_mask, dest_mask])
 
     return result
 
 
-capture = cv.VideoCapture('project_video.mp4')
+capture = cv.VideoCapture('challenge_video.mp4')
+
+History = list()
+
+LEFT_FIT = list()
+RIGHT_FIT = list()
+OFFC = list()
+WidthH = list()
+errors = 0
+
 
 while capture.isOpened():
     ret, frame = capture.read()

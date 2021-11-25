f41cd08002a17e0ed7430f3ab57e109322062dff
mrobert3456 <mrobert3456@gmail.com>
2021-11-25 21:01:07 +0100

    Optimized paralell histogram generation in parallel.py

--- parallel.py
+++ parallel.py
@@ -1,8 +1,6 @@
 import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
-import pycuda.gpuarray as gpuarray
-from pycuda import driver
 import numpy
 import os
 
@@ -58,7 +56,7 @@ def GrayScaleGPU(img):
                 G_shared[idx%1024] = G[idx%1024];
                 B_shared[idx%1024] = B[idx%1024];
                 
-
+                //if the current thread idx is inside the img boundries
                 if (row <height && col<width)
                 {
                     const unsigned int idx = col+row*width;
@@ -88,16 +86,17 @@ def GrayScaleGPU(img):
     #grid =collection of blocks
     # block cannot communicate to each other
     # block have limited shared memory, which is faster than global memory
-    block_count = (height * width - 1) / BLOCK_SIZE * BLOCK_SIZE + 1
+    block_count = (height * width - 1) / BLOCK_SIZE * BLOCK_SIZE + 1 #921600 threads
     grayConv(dev_R,
              dev_G,
              dev_B,
              numpy.uint32(width),
              numpy.uint32(height),
              block=(BLOCK_SIZE, BLOCK_SIZE, 1), #1 block has 32*32 = 1024 threads
-             grid=(dim_gridx, dim_gridy)  # Collection of blocks
-             )
+             grid=(dim_gridx, dim_gridy)  # Collection of blocks 40*23 -> so it means that we have 40*23*1024 =942080 threads
+             )                            # 942080-921600 = 20480 thread is unnecessary
 
+    # for example:
     # fullhd.jpg shape: 900*1600 so, it has = 1440000 pixels
     # 1440000/1024 = 1407 rounded -> this would be the block count
 
@@ -120,16 +119,11 @@ def GrayScaleGPU(img):
 
 
 def HistogramGPU(img):
-    res = img[0, :]
+    res = img[0, :] # histogram
     img_uj = img[int(img.shape[0] / 2):, :]
     #shape of the image : 360*1280
     height = img.shape[0] / 2
     width = img.shape[1]
-    dim_gridx = math.ceil(width / BLOCK_SIZE)
-    dim_gridy = math.ceil(height / BLOCK_SIZE)
-
-
-
 
     dev_Hist = cuda.mem_alloc(res.nbytes)
     dev_kep = cuda.mem_alloc(img_uj.nbytes)
@@ -139,42 +133,51 @@ def HistogramGPU(img):
     # Each threads sums the corresponding columns
     mod = SourceModule("""
                 __global__ void HistogramGPU(unsigned char * img,  unsigned char * Hist,const unsigned int height, const unsigned int width)
-                {
-                    //const unsigned int sor = threadIdx.y + blockIdx.y * blockDim.y;
-                    const unsigned int oszlop = threadIdx.x + blockIdx.x * blockDim.x;
+                {  
+                    const unsigned int col = threadIdx.x + blockIdx.x * blockDim.x;
+                
+                    __shared__ unsigned char shared_img[64*360];
+                    __shared__ unsigned char  shared_hist[1280];
                     
-                    //__shared__ float shared_kep[360][32];
+                    shared_hist[col%1280] = 0;
                     
-                   // if (oszlop%32 ==0)
-                    //{
-                      //for(int i=0;i<height;i++)
-                        //{
-                          //  unsigned int idx = oszlop+i*width;
-                           //shared_kep[i][oszlop%32]= img[idx];
-                       // }
-                    //}
-                    //__syncthreads();
-                    
-        
-                    if (oszlop<width)
+                    //copy 64 row to shared memory
+                    for (int j=col ;j<(col+64);j++)
                     {
-                        int sum=0;
-                        for (int i=0; i<height; i++)
+                        for (int i=0; i<360; i++)
                         {
-                            unsigned int idx = oszlop+i*width;
-                            //sum+=shared_kep[i];
-                            //atomicAdd(&shared_kep[i][oszlop%32],img[idx]);
-                          sum+=img[idx];
-                        }
-                         //*(Hist+oszlop)=sum;
-                        //int seged = Hist[oszlop];
-                        //Hist[oszlop] =seged/width;
-                       Hist[oszlop] = sum/width;
-                           
+                         unsigned int idx = j+i*width;
+                         shared_img[idx%(64*360)] = img[idx];
+                         }
                     }
-                    else
+                    __syncthreads();
+                     
+                    if (col<1280)
                     {
-                        return;
+                        unsigned int sum=0;
+                        //each thread should sum it's corresponding column
+                        for (int i=0; i<360; i++)
+                        {
+                          unsigned int idx_t = col+i*width;
+                          //atomicAdd(&shared_hist[col%1280],shared_img[idx_t%(64*360)]);
+                          sum+=int(shared_img[idx_t%(64*360)]);
+                        }
+
+                       shared_hist[col%1280] = sum/width;
+
+                        //copy back to global memory
+                       __syncthreads();
+                       if (col%64==1)
+                       {
+                            for(int i=(col-63); i<(col+64);i++)
+                            {
+                                Hist[i%1280]=shared_hist[i%1280];
+                            }
+                        
+                       }
+                    }
+                    else{
+                          return;
                     }
 
                 }
@@ -184,6 +187,7 @@ def HistogramGPU(img):
     GetHist = mod.get_function("HistogramGPU")
     GetHist(dev_kep, dev_Hist, numpy.uint32(height), numpy.uint32(width), block=(1024, 1, 1),grid=(block_count,1))
 
+    # copy from gpu to host
     Histogram = numpy.zeros_like(res)
     cuda.memcpy_dtoh(Histogram, dev_Hist)
 
